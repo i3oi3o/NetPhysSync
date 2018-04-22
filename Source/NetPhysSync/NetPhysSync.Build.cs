fc36c8aa1d89777388aee5d433d439eb204d41e5
BoBo san <i3oi3osan@gmail.com>
2018-04-22 21:44:37 +0700

    Calculate size of NPS_BUFFER_SIZE based on current physic substep setting, support roundtrip time and jitter wait physic tick.

--- Source/NetPhysSync/NetPhysSync.Build.cs
+++ Source/NetPhysSync/NetPhysSync.Build.cs
@@ -1,23 +1,72 @@
 // This is licensed under the BSD License 2.0 found in the LICENSE file in project's root directory.
 
 using UnrealBuildTool;
+using Tools.DotNETCommon;
+using System;
 
 public class NetPhysSync : ModuleRules
 {
 	public NetPhysSync(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubSystem", "OnlineSubsystemUtils", "PhysX", "APEX" });
+        
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        int BufferSize = 20;
+        FileReference ProjectFile;
 
-		// Uncomment if you are using Slate UI
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "UMG" });
+        Log.TraceInformation("######## Target Directory : "+ Target.ProjectFile.Directory.FullName);
+
+        ConfigHierarchy ConfigHeirar = ConfigCache.ReadHierarchy
+            (
+                ConfigHierarchyType.Engine,
+                DirectoryReference.FromFile(Target.ProjectFile), 
+                Target.Platform 
+            );
+
+        if (ConfigHeirar != null)
+        {
+            float PhysSubstepDeltaTime;
+            float SupportRTTInMS;
+            int JitterWaitPhysTick;
+
+            if (ConfigHeirar.TryGetValue("/Script/Engine.PhysicsSettings", "MaxSubstepDeltaTime", out PhysSubstepDeltaTime) &&
+                ConfigHeirar.TryGetValue("/Script/NPSNetSetting", "SupportRTTInMS", out SupportRTTInMS) &&
+                ConfigHeirar.TryGetValue("/Script/NPSNetSetting", "JitterWaitPhysTick", out JitterWaitPhysTick))
+            {
+                float BufferSizeFloat = (0.001f * SupportRTTInMS) / PhysSubstepDeltaTime;
+                BufferSize = (int)Math.Ceiling(BufferSizeFloat);
+
+                if(2*BufferSize > BufferSize + JitterWaitPhysTick)
+                {
+                    BufferSize = 2 * BufferSize;
+                }
+                else
+                {
+                    BufferSize = 2 * BufferSize + JitterWaitPhysTick;
+                }
+
+                Log.TraceInformation("######## NPS_BUFFER_SIZE=" + BufferSize);
+            }
+            else
+            {
+                Log.TraceInformation("######## Cannot find value from config file. Set NPS_BUFFER_SIZE to default 20");
+            }
+        }
+            
+        PublicDefinitions.Add("NPS_BUFFER_SIZE="+BufferSize);
+
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubSystem", "OnlineSubsystemUtils", "PhysX", "APEX" });
+
+
+        // Uncomment if you are using Slate UI
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "UMG" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+
 	}
 }
