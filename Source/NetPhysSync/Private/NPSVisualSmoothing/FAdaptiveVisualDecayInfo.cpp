7ec932fb6990ba212d617bdab68e4068b9c1089f
BoBo san <i3oi3osan@gmail.com>
2018-05-15 16:02:59 +0700

    Implement FAdaptiveVisualDecayInfo

---
+++ Source/NetPhysSync/Private/NPSVisualSmoothing/FAdaptiveVisualDecayInfo.cpp
@@ -0,0 +1,95 @@
+// This is licensed under the BSD License 2.0 found in the LICENSE file in project's root directory.
+
+#include "FAdaptiveVisualDecayInfo.h"
+
+FAdaptiveVisualDecayInfo::FAdaptiveVisualDecayInfo()
+	: bCachDenomiatorYet(false)
+{
+}
+
+FAdaptiveVisualDecayInfo::FAdaptiveVisualDecayInfo
+(
+	float DecayRateSmallDiffParam, 
+	float DecayRateBigDiffParam, 
+	float SnapPosSqrtThresholdParam, 
+	float SmallPosSqrtThresholdParam, 
+	float BigPosSqrtThresholdParam, 
+	float SnapRadianThresholdParam, 
+	float SmallRadianThresholdParam, 
+	float BigRadianThresholdParam
+)
+	: DecayRateSmallDiff(DecayRateSmallDiffParam)
+	, DecayRateBigDiff(DecayRateBigDiffParam)
+	, SnapPosSqrtThreshold(SnapPosSqrtThresholdParam)
+	, SmallPosSqrtThreshold(SmallPosSqrtThresholdParam)
+	, BigPosSqrtThreshold(BigPosSqrtThresholdParam)
+	, SnapRadianThreshold(SnapRadianThresholdParam)
+	, SmallRadianThreshold(SmallRadianThresholdParam)
+	, BigRadianThreshold(BigRadianThresholdParam)
+	, bCachDenomiatorYet(false)
+	, CachPosLerpDenominator(0.0f)
+	, CachRaidanLerpDenominator(0.0f)
+{
+	CachDominator();
+}
+
+FAdaptiveVisualDecayInfo::~FAdaptiveVisualDecayInfo()
+{
+}
+
+bool FAdaptiveVisualDecayInfo::CanSnapPos(const FVector& A, const FVector& B) const
+{
+	float DiffSqrt = (A - B).SizeSquared();
+	return DiffSqrt <= SnapPosSqrtThreshold;
+}
+
+bool FAdaptiveVisualDecayInfo::CanSnapRot(const FQuat& A, const FQuat& B) const
+{
+	float DiffRadian = A.AngularDistance(B);
+	return DiffRadian <= SnapRadianThreshold;
+}
+
+float FAdaptiveVisualDecayInfo::GetDecayRate(const FVector& A, const FVector& B) const
+{
+	ensureMsgf(bCachDenomiatorYet, TEXT("Please call CachDominator first."));
+	float DiffSqrt = (A - B).SizeSquared();
+	float LerpV = (DiffSqrt - SmallPosSqrtThreshold)*CachPosLerpDenominator;
+	LerpV = FMath::Clamp(LerpV, 0.0f, 1.0f);
+	return FMath::Lerp(DecayRateSmallDiff, DecayRateBigDiff, LerpV);
+}
+
+float FAdaptiveVisualDecayInfo::GetDecayRate(const FQuat& A, const FQuat& B) const
+{
+	ensureMsgf(bCachDenomiatorYet, TEXT("Please call CachDominator first."));
+	float DiffRadian = A.AngularDistance(B);
+	float LerpV = (DiffRadian - SmallRadianThreshold)*CachRaidanLerpDenominator;
+	LerpV = FMath::Clamp(LerpV, 0.0f, 1.0f);
+	return FMath::Lerp(DecayRateSmallDiff, DecayRateBigDiff, LerpV);
+}
+
+void FAdaptiveVisualDecayInfo::CachDominator()
+{
+	if (!bCachDenomiatorYet)
+	{
+		float DiffSqrtPos = BigPosSqrtThreshold - SmallPosSqrtThreshold;
+		if (DiffSqrtPos <= 0)
+		{
+			CachPosLerpDenominator = 0;
+		}
+		else
+		{
+			CachPosLerpDenominator = 1.0f / DiffSqrtPos;
+		}
+		
+		float DiffRadian = BigRadianThreshold - SmallRadianThreshold;
+		if (DiffRadian <= 0)
+		{
+			CachRaidanLerpDenominator = 0;
+		}
+		else
+		{
+			CachRaidanLerpDenominator = 1.0f / DiffRadian;
+		}
+		bCachDenomiatorYet = true;
+	}
+}
---
+++ Source/NetPhysSync/Public/NPSVisualSmoothing/FAdaptiveVisualDecayInfo.h
@@ -0,0 +1,72 @@
+// This is licensed under the BSD License 2.0 found in the LICENSE file in project's root directory.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FAdaptiveVisualDecayInfo.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct NETPHYSSYNC_API FAdaptiveVisualDecayInfo
+{
+	GENERATED_BODY()
+
+public:
+	FAdaptiveVisualDecayInfo();
+	FAdaptiveVisualDecayInfo
+	(
+		float DecayRateSmallDiffParam,
+		float DecayRateBigDiffParam,
+		float SnapPosSqrtThresholdParam,
+		float SmallPosSqrtThresholdParam,
+		float BigPosSqrtThresholdParam,
+		float SnapRadianThresholdParam,
+		float SmallRadianThresholdParam,
+		float BigRadianThresholdParam
+	);
+	~FAdaptiveVisualDecayInfo();
+
+	bool CanSnapPos(const FVector& A, const FVector& B) const;
+	bool CanSnapRot(const FQuat& A, const FQuat& B) const;
+	float GetDecayRate(const FVector& A, const FVector& B) const;
+	float GetDecayRate(const FQuat& A, const FQuat& B) const;
+
+	/**
+	 * This is supposed to call in constructor but if this data is serialized in UPROPERTY(),
+	 * We need to manually call this.
+	 */
+	void CachDominator();
+
+private:
+	UPROPERTY()
+	float DecayRateSmallDiff;
+	UPROPERTY()
+	float DecayRateBigDiff;
+	UPROPERTY()
+	float SnapPosSqrtThreshold;
+	UPROPERTY()
+	float SmallPosSqrtThreshold;
+	UPROPERTY()
+	float BigPosSqrtThreshold;
+	UPROPERTY()
+	float SnapRadianThreshold;
+	UPROPERTY()
+	float SmallRadianThreshold;
+	UPROPERTY()
+	float BigRadianThreshold;
+
+	bool bCachDenomiatorYet;
+
+	/**
+	 * Cache denominator for faster calculation.
+	 * = 1/(BigPosSqrtThreshold - SmallPosSqrtThreshold)
+	 */
+	float CachPosLerpDenominator;
+	/*
+	* Cache denominator for faster calculation.
+	* = 1/(BigRadianThreshold - SmallRaidianThreshold)
+	*/
+	float CachRaidanLerpDenominator;
+};
